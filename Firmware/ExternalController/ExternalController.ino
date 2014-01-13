0b5b5a1fbd09debb3a49cbced1c5000a4215d3d9
QuantumPhysGuy <mattwise1985@gmail.com>
2014-01-12 21:35:22 -0500

    External controller firmware created.

--- Firmware/ExternalController/ExternalController.ino
+++ Firmware/ExternalController/ExternalController.ino
@@ -1,26 +1,37 @@
 int startButton = PIN_B0;
 int selectButton = PIN_B1;
+int menuButton = PIN_D3;
+int joyPush = PIN_C6;
 int aButton = PIN_B2;
 int bButton = PIN_B3;
 int cButton = PIN_B7;
 int xButton = PIN_D0;
 int yButton = PIN_D1;
 int zButton = PIN_D2;
+int joyX = PIN_F0;
+int joyY = PIN_F1;
 
 void setup() 
 {
   pinMode(startButton, INPUT_PULLUP);
   pinMode(selectButton, INPUT_PULLUP);
+  pinMode(menuButton, INPUT_PULLUP);
+  pinMode(joyPush, INPUT_PULLUP);
   pinMode(aButton, INPUT_PULLUP);
   pinMode(bButton, INPUT_PULLUP);
   pinMode(cButton, INPUT_PULLUP);
   pinMode(xButton, INPUT_PULLUP);
   pinMode(yButton, INPUT_PULLUP);
   pinMode(zButton, INPUT_PULLUP);
+  pinMode(joyX, INPUT);
+  pinMode(joyY, INPUT);
 }
 
 void loop() 
 {
+  Joystick.X(analogRead(joyX));
+  Joystick.Y(analogRead(joyY));
+  
   if (!digitalRead(startButton))
   {
     Joystick.button(1, true);
@@ -39,7 +50,7 @@ void loop()
     Joystick.button(2, false);
   }
   
-  if (!digitalRead(aButton))
+  if (!digitalRead(menuButton))
   {
     Joystick.button(3, true);
   }
@@ -48,7 +59,7 @@ void loop()
     Joystick.button(3, false);
   }
   
-  if (!digitalRead(bButton))
+  if (!digitalRead(aButton))
   {
     Joystick.button(4, true);
   }
@@ -57,7 +68,7 @@ void loop()
     Joystick.button(4, false);
   }
   
-  if (!digitalRead(cButton))
+  if (!digitalRead(bButton))
   {
     Joystick.button(5, true);
   }
@@ -66,7 +77,7 @@ void loop()
     Joystick.button(5, false);
   }
   
-  if (!digitalRead(xButton))
+  if (!digitalRead(cButton))
   {
     Joystick.button(6, true);
   }
@@ -75,7 +86,7 @@ void loop()
     Joystick.button(6, false);
   }
   
-  if (!digitalRead(yButton))
+  if (!digitalRead(xButton))
   {
     Joystick.button(7, true);
   }
@@ -84,12 +95,30 @@ void loop()
     Joystick.button(7, false);
   }
   
-  if (!digitalRead(zButton))
+  if (!digitalRead(yButton))
   {
     Joystick.button(8, true);
   }
   else
   {
     Joystick.button(8, false);
+  }
+  
+  if (!digitalRead(zButton))
+  {
+    Joystick.button(9, true);
+  }
+  else
+  {
+    Joystick.button(9, false);
+  }
+  
+  if (!digitalRead(joyPush))
+  {
+    Joystick.button(10, true);
+  }
+  else
+  {
+    Joystick.button(10, false);
   }
 }
